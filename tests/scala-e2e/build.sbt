f1f284caf2fd649d2fc3acb8549c513b1aba0be1
Jonathan Leonard <johanatan@gmail.com>
2014-03-19 13:35:30 -0700

    Client finished (untested but compiles).

--- tests/scala-e2e/build.sbt
+++ tests/scala-e2e/build.sbt
@@ -1,4 +1,25 @@
-name := "scala-e2e"
+organization := "com.datt"
 
-version := "1.0"
-    
+name := "smartrouter-test"
+
+version := "0.1.0-SNAPSHOT"
+
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
+
+resolvers ++= Seq(
+  "spray repo" at "http://repo.spray.io"
+)
+
+seq(com.github.retronym.SbtOneJar.oneJarSettings: _*)
+
+libraryDependencies ++= {
+  val akkaV = "2.1.4"
+  val sprayV = "1.1.1"
+  Seq(
+    "io.spray"          %   "spray-client"   % sprayV,
+    "io.spray"          %%  "spray-json"     % "1.2.5",
+    "com.typesafe.akka" %%  "akka-actor"     % akkaV,
+    "org.scalatest"     %   "scalatest_2.10" % "2.0" % "test"
+  )
+}
+
---
+++ tests/scala-e2e/main/src/com/datt/smartrouter_test/Main.scala
@@ -0,0 +1,72 @@
+
+package com.datt.smartrouter_test
+
+import akka.actor.ActorSystem
+import scala.concurrent.duration._
+import akka.util.Timeout
+import spray.can.Http
+import akka.io.IO
+import akka.pattern.ask
+import spray.client.pipelining._
+import spray.http._
+import scala.concurrent.{Await, Future}
+import spray.json._
+import DefaultJsonProtocol._
+import scala.collection.mutable
+import spray.http.HttpRequest
+import spray.http.HttpResponse
+import spray.http.HttpHeaders.`Set-Cookie`
+
+object Main {
+  implicit val system = ActorSystem()
+  import system.dispatcher // execution context for futures
+
+  val timeoutDuration = 20.seconds
+
+  val cookies = mutable.Map[Integer, List[HttpCookie]]().withDefaultValue(List())
+
+  private def withCookies(client: Integer): HttpRequest => HttpRequest =
+    addHeader(HttpHeaders.Cookie(cookies(client)))
+
+  def extractCookies(res: HttpResponse) = res.headers.collect { case `Set-Cookie`(hc) => hc }
+  private def storeCookies(client: Integer): Future[HttpResponse] => Future[HttpResponse] =
+    (response) => for (res <- response) yield { cookies(client) = extractCookies(res); res }
+
+  private def issueRequest(client: Integer, hostname: String, port: Integer, req: HttpRequest): Future[HttpResponse] = {
+    implicit val timeout = Timeout.durationToTimeout(timeoutDuration)
+    val pipeline = (for (
+      Http.HostConnectorInfo(connector, _) <-
+      IO(Http) ? Http.HostConnectorSetup(hostname, port = port)
+    ) yield sendReceive(connector)).map(
+        addHeader("Accept", "application/json") ~>
+        withCookies(client)                     ~>
+        _                                       ~>
+        storeCookies(client)                    ~>
+        unmarshal[HttpResponse])
+    pipeline.flatMap(_(req))
+  }
+
+  private def getResponseJson(client: Integer, host: String, port: Integer, req: HttpRequest): JsValue = {
+    val response = issueRequest(client, host, port, Get("/"))
+    val res = Await.result(response, timeoutDuration)
+    assert(res.status == StatusCode.int2StatusCode(200))
+    res.entity.asString.asJson
+  }
+
+  def main(args: Array[String]): Unit = {
+    val Array(host, portStr) = args.head.split(":")
+
+    class RichJsVal(jsVal: JsValue) { def getField(key: String) = jsVal.asJsObject.fields(key) }
+    implicit def richJsVal(jsVal: JsValue) = new RichJsVal(jsVal)
+
+    val clients = 0 until 20
+    clients.par.foreach { client =>
+      val getServerIP = () =>
+        getResponseJson(client, host, portStr.toInt, Get("/")).getField("IP").convertTo[String]
+      val serverIP = getServerIP()
+      for (req <- 0 until 100) assert(serverIP == getServerIP())
+    }
+
+    sys.exit(0)
+  }
+}
