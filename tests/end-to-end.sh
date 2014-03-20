90e7529fff20a7f2fa77bdaa146510c4432a5b00
Jonathan Leonard <johanatan@gmail.com>
2014-03-19 22:48:41 -0700

    Use sbt package to build .jar (rather than sbt compile).

--- tests/end-to-end.sh
+++ tests/end-to-end.sh
@@ -2,7 +2,7 @@
 
 @test "requests from same client are routed to a single server" {
   pushd ./scala-e2e
-  sbt compile
+  sbt package
   popd
 
   run bash -c "java -jar ./scala-e2e/target/scala-2.10/smartRouterTest.jar" # TODO pass hostnanme:port for nginx
