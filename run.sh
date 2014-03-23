c713d3cc0707bc736b72f45ad45ac88484190a8d
John Albietz <inthecloud247@gmail.com>
2014-03-23 11:22:08 -0700

    meta-updated run and run-debug commands.

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-# not building first. assuming that it's built.
+# not building first. assuming that it's built or available remotely.
 
-docker run -i -t --rm -w "/files" --name "smartrouter" --hostname "smartrouter" datt/datt-smartrouter bash -c "supervisord; /bin/bash"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CURRENT_DIR="${DIR##*/}"
+
+docker run -i -t --rm -w "/files" --name "$CURRENT_DIR" --hostname $CURRENT_DIR loyal3/$CURRENT_DIR bash -c "supervisord && /bin/bash"
